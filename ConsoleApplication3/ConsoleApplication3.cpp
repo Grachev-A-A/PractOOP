2da94d4dd63e13015e415a813290c5134dbbb0de
Demon Tempter <sagrachov@gmail.com>
2019-04-04 15:22:45 +0400

    04.04.19

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -16,6 +16,11 @@ int index_first_palindrom(int *x, int n);
 int max_palindrom(int *x, int n, int &max);
 void insert_array(int*x, int&n, int p, int b);
 void dell_array(int*x, int&n, int p);
+void dell_palindrom(int*x, int &n);
+void dell_palindrom1(int*x, int &n);
+void sort_boobl(int*x, int n);
+int sort_boobl_good(int*x, int n);
+void sort_insert(int*x, int n);
 
 using namespace std;
 
@@ -38,44 +43,116 @@ int main()
 	out_array(a, n);
 	inp_array_d2(a, n);
 	out_array(a, n);*/
-	//Кол-во палиндромов
-	int s = count_palindrom(a, n);
-	if (s)
-		cout << "Кол-во палиндромов в массиве = " << s << endl;
-	else
-		cout << "В массиве нет палиндромов!\n";
-	// Индекс первого
-	s = index_first_palindrom(a, n);
-	if (s < 0)
-		cout << "В массиве нет палиндромов!\n";
-	else
-		cout << "Первый палиндром имеет индекс " << s << endl;
-	//Максимальный палиндром
-	int max;
-	s = max_palindrom(a, n, max);
-	if (s)
-		cout << "Максимальный палиндром имеет значение = " << max << endl;
-	else
-		cout << "В массиве нет палиндромов!\n";
+	////Кол-во палиндромов
+	//int s = count_palindrom(a, n);
+	//if (s)
+	//	cout << "Кол-во палиндромов в массиве = " << s << endl;
+	//else
+	//	cout << "В массиве нет палиндромов!\n";
+	// //Индекс первого
+	//s = index_first_palindrom(a, n);
+	//if (s < 0)
+	//	cout << "В массиве нет палиндромов!\n";
+	//else
+	//	cout << "Первый палиндром имеет индекс " << s << endl;
+	////Максимальный палиндром
+	//int max;
+	//s = max_palindrom(a, n, max);
+	//if (s)
+	//	cout << "Максимальный палиндром имеет значение = " << max << endl;
+	//else
+	//	cout << "В массиве нет палиндромов!\n";
 
-	//Вставка в массив
-	int p;
-	cout << "Index for insert: \n";
-	cin >> p;
-	if (p >= 0 && p <= n && (n + 1) < len) {
-		int b;
-		cout << "Значение для вставки:\n";
-		cin >> b;
-		insert_array(a, n, p, b);
+	////Вставка в массив
+	//int p;
+	//cout << "Index for insert: \n";
+	//cin >> p;
+	//if (p >= 0 && p <= n && (n + 1) < len) {
+	//	int b;
+	//	cout << "Значение для вставки:\n";
+	//	cin >> b;
+	//	insert_array(a, n, p, b);
+	//	out_array(a, n);
+	//	// Удаление
+	//	dell_array(a, n, p);
+	//	out_array(a, n);
+	//}
+	//else {
+	//	cout << "Позиция вне индексов массива!\n";
+	//}
+	/*dell_palindrom1(a, n);
+	if (n) 
 		out_array(a, n);
-		// Удаление
-		dell_array(a, n, p);
-		out_array(a, n);
-	}
-	else {
-		cout << "Позиция вне индексов массива!\n";
-	}
+	else
+		cout << "Массив пуст!\n";*/
 
+	//sort_boobl(a, n);
+	//out_array(a, n);
+	////inp_array(a, n);
+	//cout << "Кол-во проходов = " << sort_boobl_good(a, n) << endl;
+
+	sort_insert(a, n);
+	out_array(a, n);
+}
+
+void sort_insert(int*x, int n) {
+	int tmp, j;
+	for (int i = 1; i < n; i++) {
+		tmp = x[i];
+		j = i;
+		while (j > 0 && x[j - 1] > tmp) {
+			x[j] = x[j - 1];
+			j--;
+		}
+		x[j] = tmp;
+	}
+}
+
+void sort_boobl(int*x, int n) {
+	int tmp;
+	for (int j = 1; j < n; j++) {
+		for (int i = n - 1; i >= j; i--) {
+			if (x[i] < x[i - 1]) {
+				tmp = x[i];
+				x[i] = x[i - 1];
+				x[i - 1] = tmp;
+			}
+		}
+	}
+}
+
+int sort_boobl_good(int*x, int n) {
+	int tmp, flag;
+	for (int j = 1; j < n; j++) {
+		flag = 1;
+		for (int i = n - 1; i >= j; i--) {
+			if (x[i] < x[i - 1]) {
+				flag = 0;
+				tmp = x[i];
+				x[i] = x[i - 1];
+				x[i - 1] = tmp;
+			}
+		}
+		if (flag)
+			return j;
+	}
+}
+
+void dell_palindrom(int*x, int &n) {
+	for (int i = 0; i < n;) {
+		if (palindrom(x[i]))
+			dell_array(x, n, i);
+		else
+			i++;
+	}
+}void dell_palindrom1(int*x, int &n) {
+	int j = 0;
+	for (int i = 0; i < n; i++) {
+		x[j] = x[i];
+		if (!palindrom(x[i]))
+			j++;
+	}
+	n = j;
 }
 
 void dell_array(int*x, int&n, int p) {
@@ -95,7 +172,7 @@ int max_palindrom(int *x, int n, int &max) {
 	int flag = 0;
 	for (int i = 0; i < n; i++) {
 		if (palindrom(x[i])) {
-			if (flag == 0) {
+			if (!flag) {
 				max = x[i];
 				flag = 1;
 			}
